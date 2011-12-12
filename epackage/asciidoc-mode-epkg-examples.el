9b3f2eb32cb4e9571819621b262dc00d6c9061fc
Jari Aalto <jari.aalto@cante.net>
2011-12-12 23:05:23 +0200

    epackage/: new
    Signed-off-by: Jari Aalto <jari.aalto@cante.net>

---
+++ epackage/asciidoc-mode-epkg-examples.el
@@ -0,0 +1,15 @@
+ ;; Prevent loading this file. Study the examples.
+(error "asciidoc-mode-epkg-examples.el is not a config file.")
+
+;; Suggested add-ons doc-mode: font-lock support for asciidoc buffers
+;; http://xpt.sourceforge.net/tools/doc-mode/
+
+(autoload 'doc-mode "doc-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.adoc$" . doc-mode))
+
+(add-hook 'doc-mode-hook
+	  '(lambda ()
+	     (turn-on-auto-fill)
+	     (require 'asciidoc)))
+
+;; End of file
